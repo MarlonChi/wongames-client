ed8a6323396b52f42836b62c3013bbc21d0ec306
MarlonChi <marlonchiodelli@hotmail.com>
2023-09-10 19:52:13 -0300

    feat: verify if user in menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 
 import Logo from '../Logo'
+import Button from '../Button'
+
+import { MenuProps } from './types'
 
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
@@ -8,9 +11,8 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import * as S from './styles'
-import Button from '../Button'
 
-const Menu = () => {
+const Menu = ({ user }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
@@ -32,19 +34,30 @@ const Menu = () => {
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          {!user && (
+            <>
+              <S.MenuLink href="#">Home</S.MenuLink>
+              <S.MenuLink href="#">Explore</S.MenuLink>
+            </>
+          )}
+          {user && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
-
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="#" title="Sign In">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+        {!user && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign In">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   )
