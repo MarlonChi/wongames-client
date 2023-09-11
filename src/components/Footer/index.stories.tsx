dc9d30696563237020f22eb38a103381afd2afb3
MarlonChi <marlonchiodelli@hotmail.com>
2023-09-11 17:00:34 -0300

    feat: footer

---
+++ src/components/Footer/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Footer from '.'
+
+export default {
+  title: 'Footer',
+  component: Footer
+} as Meta
+
+export const Default: StoryObj = {}
+Default.parameters = {
+  layout: 'fullscreen'
+}
