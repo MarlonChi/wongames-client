1cf57da94ad1d428f10c783d4662f88d576ae390
MarlonChi <marlonchiodelli@hotmail.com>
2023-09-18 22:02:38 -0300

    feat: button adjusts

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -28,3 +28,11 @@ WithIcon.args = {
   size: 'medium',
   icon: <AddShoppingCart />
 }
+
+export const asLink: StoryObj = {}
+asLink.args = {
+  children: 'Buy now',
+  size: 'large',
+  as: 'a',
+  href: '/link'
+}
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,12 +1,17 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+
 export type ButtonProps = {
-  children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: React.ReactNode
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+} & ButtonTypes
 
 export type WrapperProps = { hasIcon: boolean } & Pick<
   ButtonProps,
   'size' | 'fullWidth'
 >
+
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
