7d9b87e53b500191a473f527d5c8afb6ffff194f
MarlonChi <marlonchiodelli@hotmail.com>
2023-09-22 14:43:21 -0300

    feat: Ribbon

---
+++ src/components/Ribbon/index.tsx
@@ -0,0 +1,15 @@
+import { RibbonProps } from './types'
+
+import * as S from './styles'
+
+const Ribbon = ({
+  children,
+  color = 'primary',
+  size = 'normal'
+}: RibbonProps) => (
+  <S.Wrapper color={color} size={size}>
+    {children}
+  </S.Wrapper>
+)
+
+export default Ribbon
---
+++ src/components/Ribbon/types.ts
@@ -0,0 +1,9 @@
+export type RibbonProps = {
+  children: React.ReactNode
+  color?: RibbonColors
+  size?: RibbonSizes
+}
+
+export type RibbonColors = 'primary' | 'secondary'
+
+export type RibbonSizes = 'normal' | 'small'
