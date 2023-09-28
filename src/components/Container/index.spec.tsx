08381b39b552865dc488c62b29ad8d6c75912132
MarlonChi <marlonchiodelli@hotmail.com>
2023-09-27 21:04:38 -0300

    feat: Container

---
+++ src/components/Container/index.spec.tsx
@@ -0,0 +1,21 @@
+import { renderWithTheme } from '../../utils/tests/helpers'
+import theme from '../../styles/theme'
+
+import { Container } from '.'
+
+describe('<Container/>', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Won Games</span>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
