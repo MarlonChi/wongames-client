d173e6fb586dc7523b2fc9f51983175f08e4ad6f
MarlonChi <marlonchiodelli@hotmail.com>
2023-10-16 17:14:56 -0300

    feat: Base template

---
+++ src/templates/Base/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Base from '.'
+
+export default {
+  title: 'Pages/Base',
+  component: Base
+} as Meta
+
+export const Default: StoryObj = {}
---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,23 @@
+import { Container } from '../../components/Container'
+import Menu from '../../components/Menu'
+import Footer from '../../components/Footer'
+
+import { BaseTemplateProps } from './types'
+
+import * as S from './styles'
+
+const Base = ({ children }: BaseTemplateProps) => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+    {children}
+    <S.SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </S.SectionFooter>
+  </section>
+)
+
+export default Base
