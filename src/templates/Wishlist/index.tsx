7e4498122a9d8d78f892c4d610a9c8a3606ca9ab
MarlonChi <marlonchiodelli@hotmail.com>
2023-10-26 22:33:02 -0300

    feat: Grid component

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -3,11 +3,12 @@ import Heading from '../../components/Heading'
 import Showcase from '../../components/Showcase'
 import GameCard from '../../components/GameCard'
 
+import { Grid } from '../../components/Grid'
 import { Container } from '../../components/Container'
 
 import { WishlistTemplateProps } from './types'
 
-import * as S from './styles'
+// import * as S from './styles'
 
 const Wishlist = ({
   games,
@@ -20,9 +21,11 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      {games?.map((game, index) => (
-        <GameCard key={`wishlist-${index}`} {...game} />
-      ))}
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Grid>
     </Container>
     <Showcase
       title="You may like these games"
