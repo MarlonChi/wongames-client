95753d947301067b8b68f5335507638679508bcc
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-03 16:35:38 -0300

    feat: My orders page

--- src/app/profile/orders/page.tsx
+++ src/app/profile/orders/page.tsx
@@ -1,7 +1,22 @@
 'use client'
 
+import OrdersList from '../../../components/OrdersList'
 import Profile from '../../../templates/Profile'
 
+import ordersMock from '../../../components/OrdersList/mock'
+
 export default function Orders() {
-  return <Profile>Wait</Profile>
+  return (
+    <Profile>
+      <OrdersList items={ordersMock} />
+    </Profile>
+  )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      cards: ordersMock
+    }
+  }
 }
