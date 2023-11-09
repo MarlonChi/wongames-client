263fe476ba6f8afbb25d94c7c0d4c9256c5243d8
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-08 21:36:21 -0300

    feat: CartList adjusts

--- src/components/CartList/types.ts
+++ src/components/CartList/types.ts
@@ -1,7 +1,11 @@
 import { GameItemProps } from '../GameItem/types'
 
 export type CartListProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
   hasButton?: boolean
+}
+
+export type WrapperProps = {
+  isEmpty: boolean
 }
