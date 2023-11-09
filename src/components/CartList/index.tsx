940d03aa23dde6ed4f8aa9fec4973706ce3b83a5
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-08 21:02:47 -0300

    feat: CartList update

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,15 +1,24 @@
+import Link from 'next/link'
+
 import GameItem from '../GameItem'
+import Button from '../Button'
 
 import { CartListProps } from './types'
 
 import * as S from './styles'
 
-const CartList = ({ items, total }: CartListProps) => (
+const CartList = ({ items, total, hasButton = false }: CartListProps) => (
   <S.Wrapper>
     {items?.map((item) => <GameItem key={item.title} {...item} />)}
 
     <S.Footer>
-      Total <S.Total>{total}</S.Total>
+      {!hasButton && <span>Total: </span>}
+      <S.Total>{total}</S.Total>
+      {hasButton && (
+        <Link href="/cart">
+          <Button as="a">Buy it now</Button>
+        </Link>
+      )}
     </S.Footer>
   </S.Wrapper>
 )
